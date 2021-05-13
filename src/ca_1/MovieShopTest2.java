c337b8edd53e66be3c37bc56662720d7fdb3af26
Dhruv Sandoval <dhruvsandoval@gmail.com>
2021-05-13 20:32:52 +0100

    MovieShopTest Fixed

--- src/ca_1/MovieShopTest2.java
+++ src/ca_1/MovieShopTest2.java
@@ -1,6 +1,7 @@
 package ca_1;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -27,9 +28,8 @@ class MovieShopTest2 {
 	@BeforeEach
 	void setUp() throws Exception {
 	    testShop = new MovieShop();
-	    System.out.print("PLEASE ADD 1 MOVIE TO THE TEST ARRAY" + "\n\n\n\n");
+	    System.out.print("PLEASE ADD 2 MOVIES TO THE TEST ARRAY" + "\n\n\n\n");
 
-	    // add a movie
 	    System.out.print("Enter movie title: ");
 	    String title = EasyScanner.nextString();
 
@@ -45,13 +45,48 @@ class MovieShopTest2 {
 	    System.out.print("Add a short plot for the movie: ");
 	    String plot = EasyScanner.nextString();
 
+	    System.out.print("Add a short name for the actor: ");
+	    String name = EasyScanner.nextString();
+	    
+	    System.out.print("Add a DoB for the actor: ");
+	    String dob = EasyScanner.nextString();
+	    
+	    System.out.print("Add a gender for the actor: ");
+	    String gender = EasyScanner.nextString();
+	    
+	    System.out.print("Add a nationality for the actor: ");
+	    String nationality = EasyScanner.nextString();
+	    
+	    System.out.print("Add a character name for the actor: ");
+	    String character = EasyScanner.nextString();
+	    
 	    testShop.addMovie(new Movie(title, yearOfRelease, runningTime, genre, plot));
 	    System.out.print(title + " was added successfully" + "\n");
+
+	    testShop.addActor(new Actor(name, dob, gender, nationality));
+	    System.out.print("The actor " + name + " was added successfully" + "\n");
 	    
-	    System.out.print("Add a name for the actor");
+	    testShop.addCharacters(new Character(Actor, Movie, character));
+	    
+	    System.out.print("Enter movie title: ");
+	    String title2 = EasyScanner.nextString();
 
-	}
+	    System.out.print("Enter year of release: ");
+	    int yearOfRelease2 = EasyScanner.nextInt();
 
+	    System.out.print("Enter the running time: ");
+	    Float runningTime2 = EasyScanner.nextFloat();
+
+	    System.out.print("Enter the movies genre: ");
+	    String genre2 = EasyScanner.nextString();
+
+	    System.out.print("Add a short plot for the movie: ");
+	    String plot2 = EasyScanner.nextString();
+
+	    testShop.addMovie(new Movie(title2, yearOfRelease2, runningTime2, genre2, plot2));
+	    System.out.print(title2 + " was added successfully" + "\n\n\n\n");
+
+	    }
 
 	@AfterEach
 	void tearDown() throws Exception {
@@ -113,13 +148,10 @@ class MovieShopTest2 {
 
 	}
 
-
-
-//
-//	@Test
-//	void testDisplayMovieAndActors() {
-//	    fail("Not yet implemented");
-//	}
+	@Test
+	void testDisplayMovieAndActors() {
+	    fail("Not yet implemented");
+	}
 
 
 }
